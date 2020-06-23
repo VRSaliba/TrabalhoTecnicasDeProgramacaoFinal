791232815cdd4fd8f9d3623bb411b2f8fd414a64
VRSaliba <62573292+VRSaliba@users.noreply.github.com>
2020-06-22 22:23:16 -0300

    Add files via upload

---
+++ Bookflix/src/main/java/com/Group9/Bookflix/BookflixApplication.java
@@ -0,0 +1,13 @@
+package com.Group9.Bookflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflixApplication.class, args);
+	}
+
+}
